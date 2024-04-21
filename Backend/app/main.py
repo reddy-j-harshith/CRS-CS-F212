0b6282aee269737376ce9292929fee5946a0e07c
reddy-j-harshith <143481508+reddy-j-harshith@users.noreply.github.com>
2024-04-21 12:22:12 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,7 +2,7 @@ import cx_Oracle as cx
 from pydantic import ValidationError
 from fastapi import FastAPI, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from .models import Query, Rental, User, Bicycle
+from .models import Query, User, Bicycle, Extension, Feedback, Rental, Confirmer, deleteUser, deleteBicycle
 from datetime import datetime
 
 app = FastAPI()
@@ -17,7 +17,12 @@ app.add_middleware(
 
 connStr = 'crs/crs123@localhost:1521/xepdb1'
 
-# Functions
+
+
+# HELPER FUNCTIONS
+
+
+
 def get_connection():
     try:
         conn = cx.connect(connStr)
@@ -63,16 +68,20 @@ def create_bicycle_procedure(bicycle_type, lender_id, model_type, colors):
         cursor.close()
         conn.close()
 
-        return generated_bicycle_id
-
     except cx.DatabaseError as e:
         print("error:", e)
         raise
 
 
 
+
+# QUERYING INTERFACE
+
+
+
+
 # Basic Querying interface of the database
-@app.post("/query", status_code=status.HTTP_200_OK)
+@app.post("/query", status_code=status.HTTP_201_CREATED)
 def query(query: Query):
     conn = get_connection()
     try:
@@ -97,8 +106,13 @@ def query(query: Query):
 
 
 
-# Insertion into the Rental Table
-@app.post("/rental", status_code=status.HTTP_200_OK)
+
+# INSERTION AND CONFIRMATION OF THE RENTAL
+
+
+
+
+@app.post("/rental", status_code=status.HTTP_201_CREATED)
 async def create_rental_record(rental: Rental):
     try:
         conn = get_connection()
@@ -118,8 +132,32 @@ async def create_rental_record(rental: Rental):
         cursor.close()
         conn.close()  
 
+@app.post("/rent-confirm", status_code=status.HTTP_202_ACCEPTED)
+def confirm(confirmer: Confirmer):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
 
-@app.post("/create-user")
+        rtn_date = datetime.strptime(confirmer.return_date, "%Y-%m-%d")
+
+        cursor.callproc("confirm_rental", [confirmer.rental_id, rtn_date, confirmer.damaged_flag])
+
+    except ValidationError as e:
+        return HTTPException(status_code=422, detail=str(e))
+    
+    except cx.DatabaseError as e:
+        print("Database error:", e)
+        raise
+
+
+
+
+# CREATE AND REMOVE A USER FROM THE PORTAL
+
+
+
+
+@app.post("/create-user", status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
     try:
         user_data = user.dict()
@@ -134,16 +172,90 @@ async def create_user(user: User):
         print("Database error:", e)
         raise
     
+@app.post("/delete-user", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user: deleteUser):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
 
-@app.post("/create-bicycle")
+        cursor.callproc("delete_user", [user.user_id])
+
+        conn.commit()
+
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))        
+
+
+
+# CREATE AND REMOVE A BICYCLE FROM THE RECORDS
+
+
+
+
+@app.post("/create-bicycle", status_code=status.HTTP_201_CREATED)
 async def create_bicycle(bicycle: Bicycle):
     try:
         bicycle_data = bicycle.dict()
         
-        bicycle_id = create_bicycle_procedure(**bicycle_data)
+        create_bicycle_procedure(**bicycle_data)
         
-        return {"message": "Bicycle created successfully with ID: {}".format(bicycle_id)}
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
     
+    except cx.DatabaseError as e:
+        print("Database error:", e)
+        raise
+
+@app.post("/delete-bicycle", status_code=status.HTTP_204_NO_CONTENT)
+async def create_bicycle(bicycle: deleteBicycle):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        
+        cursor.callproc("delete_bicycle", [bicycle.bicycle_id])
+        conn.commit()
+        
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    
+    except cx.DatabaseError as e:
+        print("Database error:", e)
+        raise
+
+
+
+
+
+# FEEDBACK AND EXTENSION
+
+
+
+@app.post("/feedback", status_code=status.HTTP_201_CREATED)
+def enter_feedback(feedback: Feedback):
+    conn = get_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.callproc("enter_feedback", [feedback.user_id, feedback.rating, feedback.comments])
+
+        conn.commit()
+
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    
+    except cx.DatabaseError as e:
+        print("Database error:", e)
+        raise 
+
+@app.post("/extension", status_code=status.HTTP_201_CREATED)
+def request_extension(extension: Extension):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.callproc("create_extension_record", [extension.rental_id, extension.extra_duration])
+        
+        conn.commit()
+
     except ValidationError as e:
         raise HTTPException(status_code=422, detail=str(e))
     
